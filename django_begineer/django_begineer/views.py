0effcadc5722b5550be0dd8acf6fdd09aebf2ea0
Darpan Neve <Darpanneve3@gmail.com>
2024-07-10 21:50:38 +0530

    init

---
+++ django_begineer/django_begineer/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    # return HttpResponse("Hello, Django!")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("About Django")
+
+def contact(request):
+    return HttpResponse("Contact Django")
